9c8b1c6db8f766c21d2dfd2c21f49f8e1e0e5c0f
Gary Szekely <szekely.gary2000@gmail.com>
2023-06-07 19:54:50 -0400

    commit

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    
+</script>
+
+<template>
+    <nav class="p-3 bg-neutral dark:bg-neutral-950">
+        <h1 class="text-3xl text-white font-bold">Kitchen</h1>
+    </nav>
+    <div class="flex-1 bg-white dark:bg-neutral-900">
+
+    </div>
+</template>
+
+<style scoped>
+    
+</style>
