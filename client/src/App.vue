e4ac159ee074fa583ac894eaca4255ca8f2c0c02
Gary Szekely <szekely.gary2000@gmail.com>
2023-06-09 19:17:27 -0400

    connected client to usda database

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,39 @@
 <script setup lang="ts">
-    
+</script>
+
+<script lang="ts">
+
+export default {
+    data() {
+        return {
+            item: '',
+            items: []
+        }
+    },
+    methods: {
+        async search(e: Event) { this.items = (await (await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=Lk3iBc7LT1vWeyRuRPrlof9n639DqmLkyd8ROLUv&query=${this.item}`)).json())['foods']; }
+    }
+}
 </script>
 
 <template>
-    <nav class="p-3 bg-neutral dark:bg-neutral-950">
-        <h1 class="text-3xl text-white font-bold">Kitchen</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <div class="navbar-brand fw-bold">Kitchen</div>
+            <button class="btn btn-danger fw-bold">Log Out</button>
+        </div>
     </nav>
-    <div class="flex-1 bg-white dark:bg-neutral-900">
-
+    <div class="container-fluid p-3">
+        <div class="card">
+            <div class="card-body">
+                <label class="form-label fw-bold">Select Food/Drink:</label>
+                <input v-model="item" @input="search" list="food/drink" class="form-control">
+                <datalist id="food/drink">
+                    <option v-for="item in items">{{ item['description'] }}</option>
+                </datalist>
+            </div>
+        </div>
     </div>
 </template>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,8 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './assets/main.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 createApp(App).mount('#app');
